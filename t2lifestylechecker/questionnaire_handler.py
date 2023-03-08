0884c467b99ee0edf28cba3f518beefad819b6d7
a.nettleship@gmail.com <a.nettleship@gmail.com>
2023-03-08 10:12:25 +0000

    T test_QuestionnaireHandler_validate_question_data_should_set_questionnaire_validity_to_valid_for_valid_question_data

--- t2lifestylechecker/questionnaire_handler.py
+++ t2lifestylechecker/questionnaire_handler.py
@@ -1,12 +1,26 @@
 import json
+from enum import Enum
+
+
+questionnaire_validity_states = Enum(
+    'questionnaire_validity', [
+        'not_checked',
+        'not_valid',
+        'valid'
+    ]
+)
+
 
 class QuestionnaireHandler():
 
     def __init__(self):
         self.question_data = None
+        self.questionnaire_validity = questionnaire_validity_states['not_checked']
 
     def load_question_data(self, question_data_path):
         with open(question_data_path, 'r') as f:
             self.question_data = json.load(f)
 
+    def validate_question_data(self):
 
+        self.questionnaire_validity = questionnaire_validity_states['valid']
